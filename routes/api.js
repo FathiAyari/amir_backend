bdba5aa928238160c35227b03228742e8bba1f7f
FathiAyari <fathi.ayari@kelibia.r-iset.tn>
2023-03-20 21:09:58 +0100

    vercel config

--- routes/api.js
+++ routes/api.js
@@ -10,7 +10,7 @@ const authController=require('../controllers/auth_controller')
 
 router.post('/register',authController.register);
 router.post('/login',authController.login);
-router.get('/',function (req, res){
+router.get('/home',function (req, res){
     res.json('welcome to amir backend apis')
 });
 
