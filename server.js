155335a965ac6b2c05266bc38355aae850a51ac5
FathiAyari <fathi.ayari@kelibia.r-iset.tn>
2023-03-20 21:29:17 +0100

    vercel config

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
 const router = require('./routes/api');
 const uri = process.env.MONGODB_URI;
 
-mongoose.connect(uri,
+/*mongoose.connect(uri,
     {
         useNewUrlParser: true,
 
@@ -23,8 +23,11 @@ connection.on("connected", function () {
     console.log("connected");
 });
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use("/", router);
+app.use(bodyParser.json());*/
+/*app.use("/", router);*/
+app.get('/home',function (req, res){
+    res.json('welcome to amir backend apis')
+});
 
 app.listen(PORT, function (err) {
     if (err) console.log("Error in server setup")
