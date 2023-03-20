c46f91b70f29ddc28aef5a3f356785f6ba29406f
FathiAyari <fathi.ayari@kelibia.r-iset.tn>
2023-03-20 20:54:43 +0100

    add welcome api endpoint

--- routes/api.js
+++ routes/api.js
@@ -10,6 +10,9 @@ const authController=require('../controllers/auth_controller')
 
 router.post('/register',authController.register);
 router.post('/login',authController.login);
+router.post('/',function (req, res){
+    res.json('welcome to amir backend apis')
+});
 
 
 
